c7a985cc4a20007219e932216aa235f46a75fa17
girishv6991 <girishv6991@gmail.com>
2018-05-16 11:24:54 -0700

    Initial commit

---
+++ src/GridDb.Core/DemoData.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace GridDb.Core
+{
+    public class DemoData : INotifyPropertyChanged, IEditableObject
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                RaisePropertyChanged("Name");
+            }
+        }
+
+        private double _price;
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                _price = value;
+                RaisePropertyChanged("Price");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void RaisePropertyChanged(string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        public void BeginEdit()
+        {
+            // throw new NotImplementedException();
+        }
+
+        public void CancelEdit()
+        {
+            // throw new NotImplementedException();
+        }
+
+        public void EndEdit()
+        {
+            // throw new NotImplementedException();
+        }
+    }
+}
--- src/GridDb.Core/ViewModels/Home/HomeViewModel.cs
+++ src/GridDb.Core/ViewModels/Home/HomeViewModel.cs
@@ -6,5 +6,28 @@ namespace GridDb.Core.ViewModels.Home
 {
     public class HomeViewModel : BaseViewModel
     {
+        private List<DemoData> _demoCollection;
+        public List<DemoData> DemoCollection
+        {
+            get => _demoCollection;
+            set => SetProperty(ref _demoCollection, value);
+        }
+
+        public HomeViewModel()
+        {
+            SetupDemoData();
+        }
+
+        private void SetupDemoData()
+        {
+            var collection = new List<DemoData>();
+            var i = 1;
+            while (i <= 10)
+            {
+                collection.Add(new DemoData() { Name = $"Demo {i}", Price = 2.25 * i });
+                i += 1;
+            }
+            DemoCollection = collection;
+        }
     }
 }
